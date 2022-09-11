2f8a1f4b224718a53b8c465e824dfa9bf48142a2
Lar May See <larmaysee.git@gmail.com>
2022-09-12 00:24:34 +0630

    OnRefresh Onstart

--- src/main/java/com/jdc/event/FrameworkLifeCycleListener.java
+++ src/main/java/com/jdc/event/FrameworkLifeCycleListener.java
@@ -3,17 +3,45 @@ package com.jdc.event;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.springframework.context.ApplicationEvent;
-import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.ContextStartedEvent;
+import org.springframework.context.event.ContextStoppedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 @Component
-public class FrameworkLifeCycleListener implements ApplicationListener<ApplicationEvent>{
+public class FrameworkLifeCycleListener {
 
-	public void onApplicationEvent(ApplicationEvent event) {
+	@EventListener(ContextRefreshedEvent.class)
+	public void onRefr(ContextRefreshedEvent event) {
 		var date = new Date(event.getTimestamp());
 		var dateString = new SimpleDateFormat("yyyy/MM/dd HH:mm ss SSS").format(date);	
 		
-		System.out.println("Event is occured at %s".formatted(dateString));
+		System.out.println("IoC refresh at %s".formatted(dateString));
+	}
+	
+	@EventListener(ContextClosedEvent.class)
+	public void onClose(ContextClosedEvent event) {
+		var date = new Date(event.getTimestamp());
+		var dateString = new SimpleDateFormat("yyyy/MM/dd HH:mm ss SSS").format(date);	
+		
+		System.out.println("IoC Close at %s".formatted(dateString));
+	}
+	
+	@EventListener(ContextStartedEvent.class)
+	public void onStart(ContextStartedEvent event) {
+		var date = new Date(event.getTimestamp());
+		var dateString = new SimpleDateFormat("yyyy/MM/dd HH:mm ss SSS").format(date);	
+		
+		System.out.println("IoC Start at %s".formatted(dateString));
+	}
+	
+	@EventListener(ContextStoppedEvent.class)
+	public void onStop(ContextStoppedEvent event) {
+		var date = new Date(event.getTimestamp());
+		var dateString = new SimpleDateFormat("yyyy/MM/dd HH:mm ss SSS").format(date);	
+		
+		System.out.println("IoC Stop at %s".formatted(dateString));
 	}
 
 }
